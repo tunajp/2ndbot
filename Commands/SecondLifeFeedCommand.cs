6d7497d7c97ea5c0ffe5b2332a6e5edf8e2c312e
tunajp <tunajp@gmail.com>
2022-04-22 22:36:41 +0900

    second life Incidents

--- Commands/SecondLifeFeedCommand.cs
+++ Commands/SecondLifeFeedCommand.cs
@@ -3,6 +3,7 @@ using System.Xml.Linq;
 namespace SecondBot.Client {
     static public class SecondLifeFeedCommand {
         public static async Task<String> feed() {
+            string msg = "";
             try {
                 using (var client = new HttpClient()) {
                     var URL = "https://api.secondlife.com/datafeeds/secondlife.xml";
@@ -14,7 +15,7 @@ namespace SecondBot.Client {
                         Console.WriteLine(xml?.Element("status")?.Value);
                         Console.WriteLine(xml?.Element("signups")?.Value);
                         Console.WriteLine(xml?.Element("inworld")?.Value);
-                        return "SecondLifeについて、サーバーは" + xml?.Element("status")?.Value + "、" + "アカウント作成数は" + xml?.Element("signups")?.Value + "、" + "現在ログイン中の廃人は" + xml?.Element("inworld")?.Value + "人います。";
+                        msg = "SecondLifeについて、サーバーは" + xml?.Element("status")?.Value + "、" + "アカウント作成数は" + xml?.Element("signups")?.Value + "、" + "現在ログイン中の廃人は" + xml?.Element("inworld")?.Value + "人います。\n";
                     } else {
                         Console.WriteLine("error");
                         return "error";
@@ -24,7 +25,31 @@ namespace SecondBot.Client {
                 Console.WriteLine(e.Message);
                 return e.Message;
             }
-            return "";
+            // チャットが騒がしくなりすぎるのでコメントアウト
+            //try {
+            //    using (var client = new HttpClient()) {
+            //        var URL = "https://status.secondlifegrid.net/history.rss";
+            //        var response = await client.GetAsync(URL);
+            //        if(response.IsSuccessStatusCode) {
+            //            var _response = await response.Content.ReadAsStringAsync();
+            //            var document = XDocument.Parse(_response);
+            //            XElement? xml = document.Root;
+            //            XElement? channel = xml.Element("channel");
+            //            IEnumerable<XElement>? items = channel?.Elements("item");
+            //            foreach(var item in items) {
+            //                msg += item.Element("title").Value;
+            //                msg += " " + item.Element("link").Value;
+            //            }
+            //        } else {
+            //            Console.WriteLine("error");
+            //            msg += "error";
+            //        }
+            //    }
+            //} catch (Exception e) {
+            //    Console.WriteLine(e.Message);
+            //    msg += e.Message;
+            //}
+            return msg;
         }
     }
 }
